0f6ead842fe68f9455a9c2cc1ebeac60921cfa5e
yesharry <reerohh@gmail.com>
2023-05-23 16:08:44 +0900

    Add: create, list 기능 구현

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
+
 const CreateToDo = () => {
-  return <div>CreateToDo</div>;
+  const setToDos = useSetRecoilState(toDoState);
+  const category = useRecoilValue(categoryState);
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!value) {
+      alert("Please write a to do");
+      return;
+    }
+    setToDos((oldToDo) => [
+      { text: value, id: Date.now(), category: category },
+      ...oldToDo,
+    ]);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={value} placeholder="Write a to do" />
+      <button>ADD</button>
+    </form>
+  );
 };
 
 export default CreateToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,45 @@
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import { Categories, categoryState, toDoSelector } from "../atoms";
+import ToDo from "./ToDo";
+
 const ToDoList = () => {
-  return <div>ToDoList</div>;
+  const category = useRecoilValue(categoryState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+
+  return (
+    <div>
+      <h1>TO DO LIST</h1>
+      <hr />
+      <CreateToDo />
+
+      <h2>To Do</h2>
+      {category === Categories.TO_DO && (
+        <ul>
+          {toDo.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </ul>
+      )}
+      <hr />
+
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
 };
 
 export default ToDoList;
